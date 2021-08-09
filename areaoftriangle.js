a53297f0438e2c0311361bc117738a7e627bc859
Nishant-96 <nishant11pratihast@gmail.com>
2021-08-09 16:14:19 +0530

    refactor: error box

--- areaoftriangle.js
+++ areaoftriangle.js
@@ -7,27 +7,27 @@ var errorBox = document.querySelector("#error-box");
 
 calculateButton.addEventListener("click", clickHandler);
 
-function clickHandler(){
+function clickHandler() {
     var baseOfTriangle = Number(base.value);
     var heightOfTriangle = Number(height.value);
 
 
-    errorBox.style.display = "none";             
-    outputBox.style.display = "none";          
-    
-    if(baseOfTriangle>0 && heightOfTriangle>0){
-        var areaOfTriangle = calculateArea(baseOfTriangle,heightOfTriangle);
-        console.log(areaOfTriangle)
-        outputBox.style.display = "block";  
-        outputBox.innerText="The area of the triangle is: "+areaOfTriangle;
-    }else{
-        alert("Value Error: Input should be number and more than zero.")
+    errorBox.style.display = "none";
+    outputBox.style.display = "none";
+
+    if (baseOfTriangle > 0 && heightOfTriangle > 0) {
+        var areaOfTriangle = calculateArea(baseOfTriangle, heightOfTriangle);
+
+        outputBox.style.display = "block";
+        outputBox.innerText = "The area of the triangle is: " + areaOfTriangle;
+    } else {
+        alert("Input Value Error: Input should be a number and more than zero.")
         errorBox.style.display = "block";
-        errorBox.innerText="Please Enter Valid values in both the fields."
-    }   
+        errorBox.innerText = "Please Enter Valid values in both the fields."
+    }
 }
 
-function calculateArea(b,h){
-    var area = (b*h)/2;
+function calculateArea(b, h) {
+    var area = (b * h) / 2;
     return area;
 }
