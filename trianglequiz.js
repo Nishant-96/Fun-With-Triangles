29ed176aae2bfc6b9e5bf7d60270735aa1bdcbc8
Nishant-96 <nishant11pratihast@gmail.com>
2021-08-10 13:27:35 +0530

    update: added error handling for input
    error handling if all inputs are not given.

--- trianglequiz.js
+++ trianglequiz.js
@@ -1,6 +1,7 @@
 var quizForm = document.querySelector("#quiz-form");
 var submitButton = document.querySelector("#submit-button");
 var outputBox = document.querySelector("#output-box-quiz");
+var errorBox=document.querySelector("#error-box-quiz")
 
 submitButton.addEventListener("click",clickHandler)
 
@@ -12,12 +13,25 @@ function clickHandler(){
     var score = 0;
     var i=0;
     var userAnswers = new FormData(quizForm)
+    var userResponses=[];
+
+    errorBox.style.display="none";
+    outputBox.style.display="none";
     
     for(let value of userAnswers.values()){
-        if(value === correctAnswers[i]){
-            score=score+1;
-        }
-            i++;
+        userResponses.push(value);        
     }
-    outputBox.innerText="You have answered a total of "+score+" questions correctly. Your total score is: "+ score+".";
+    if(userResponses.length===5){
+        for(i=0;i<userResponses.length;i++){
+            if(userResponses[i]===correctAnswers[i]){
+                score=score+1;
+            }
+        }
+        outputBox.style.display="block";
+        outputBox.innerText="You have answered a total of "+score+" questions correctly. Your total score is: "+ score+".";
+    }else{
+        errorBox.style.display= "block";
+        errorBox.innerHTML= `<div style="background-color: red; border-radius:0.5rem; padding: 0.5rem; margin-top:1rem;font-weight: bolder;text-align:center">Oops! It seems you missed out on some question. Please answer all of the questions before submitting.</div>`
+    }
+    
 }
